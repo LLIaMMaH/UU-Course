2f143a47f5b7f83cbe8b6442beb16df529695140
LLIaMMaH <abratunets@gmail.com>
2024-11-11 20:26:46 +0300

    Added Module 01 main3.py file

---
+++ Module_01/main3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def task_1() -> None:
+    example = 'А роза упала на лапу Азора'
+    print(example[0])
+    print(example[-1])
+    print(example[(len(example) - 1) // 2:])
+    print(example[::-1])
+    print(example[::2])
+
+
+if __name__ == "__main__":
+    task_1()
